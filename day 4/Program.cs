e285dc332c6a01ef107d461bf87f1b9bb2deebb9
sprz <sprzybycien@gmail.com>
2017-12-07 23:13:54 +0100

    day 4 part 1

---
+++ day 4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace day_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+             var lines = File.ReadAllLines("data.txt");
+            var count = 0;
+             foreach (var line in lines)
+             {
+                 var isValid = true;
+                 HashSet<string> uniquePasswords = new HashSet<string>();
+                 foreach (var word in line.Split(' '))
+                 {
+                     isValid &= !uniquePasswords.Contains(word);
+                        if(!isValid) break;
+                        uniquePasswords.Add(word);
+
+
+                 }
+                 if(isValid) count ++;
+             }
+             Console.WriteLine(count);
+        }
+    }
+}
