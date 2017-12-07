77fff4e2327bb9d13b2302eb28df40668c2e3bc2
Sławomir Przybycień <SPrzybycien@Metegrity.com>
2017-12-07 18:30:51 +0100

    initial day3 - part2

--- day 3/Program.cs
+++ day 3/Program.cs
@@ -7,18 +7,36 @@ namespace day_3
         static void Main(string[] args)
         {
 
-           Console.WriteLine(CalculateDistance(12) == 3);
-           Console.WriteLine(CalculateDistance(23) == 2);
-           Console.WriteLine(CalculateDistance(25) == 4);
-           Console.WriteLine(CalculateDistance(27) == 4);
-           Console.WriteLine(CalculateDistance(28) == 3);
-           Console.WriteLine(CalculateDistance(29) == 4);
-           Console.WriteLine(CalculateDistance(30) == 5);
-           Console.WriteLine(CalculateDistance(31) == 6);
-           Console.WriteLine(CalculateDistance(50) == 7);
-           Console.WriteLine(CalculateDistance(49) == 6);
-           Console.WriteLine(CalculateDistance(1024) == 31);
-           Console.WriteLine(CalculateDistance(361527));
+        //    Console.WriteLine(CalculateDistance(12) == 3);
+        //    Console.WriteLine(CalculateDistance(23) == 2);
+        //    Console.WriteLine(CalculateDistance(25) == 4);
+        //    Console.WriteLine(CalculateDistance(27) == 4);
+        //    Console.WriteLine(CalculateDistance(28) == 3);
+        //    Console.WriteLine(CalculateDistance(29) == 4);
+        //    Console.WriteLine(CalculateDistance(30) == 5);
+        //    Console.WriteLine(CalculateDistance(31) == 6);
+        //    Console.WriteLine(CalculateDistance(50) == 7);
+        //    Console.WriteLine(CalculateDistance(49) == 6);
+        //    Console.WriteLine(CalculateDistance(1024) == 31);
+        //    Console.WriteLine(CalculateDistance(361527));
+
+
+
+           Console.WriteLine(Part2(1) == 1);
+           Console.WriteLine(Part2(3) == 4);
+           Console.WriteLine(Part2(5) == 10);
+           Console.WriteLine(Part2(6) == 10);
+           Console.WriteLine(Part2(7) == 10);
+           Console.WriteLine(Part2(8) == 10);
+           Console.WriteLine(Part2(9) == 10);
+           Console.WriteLine(Part2(10) == 11);
+           Console.WriteLine(Part2(99) == 122);
+           Console.WriteLine(Part2(146) == 147);
+           Console.WriteLine(Part2(147) == 304);
+           Console.WriteLine(Part2(700) == 747);
+           Console.WriteLine(Part2(400) == 747);
+           
+           Console.WriteLine(Part2(361527));
         }
 
 
@@ -34,5 +52,9 @@ namespace day_3
             return (int) (distToCenterOfSide+circleNum);
         }
         
+        public static int Part2(int value){
+
+            return -1;
+        }
     }
 }
