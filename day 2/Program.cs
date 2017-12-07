0d25eebb911f875495bc1600d4920fb1fe6f2da0
Sławomir Przybycień <SPrzybycien@Metegrity.com>
2017-12-07 16:31:21 +0100

    day 2 part 2

---
+++ day 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace day_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            var text = System.IO.File.ReadLines("data.txt");
+            short count = 0;
+            foreach(var line in text)
+            {
+                var values = line.Split("\t").Select( x=> Convert.ToInt16(x));
+                Console.WriteLine(values);
+
+                foreach(var number in values.Where(x=> x < values.Max()/2))
+                {
+                    var theNumber = values.Where( x=> x % number == 0 && x != number);
+                    if(theNumber.Count() == 1)
+                    {
+                        count += (short) (theNumber.First() / number);
+                        break;
+                    }
+                }
+            }
+            
+            count += 0;
+        }
+    }
+}
