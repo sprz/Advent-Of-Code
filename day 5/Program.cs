78be4d202cd0819b56186757eaaa8d6ed4e50adc
Sławomir Przybycień <SPrzybycien@Metegrity.com>
2017-12-08 10:42:19 +0100

    day 5 part 1  + part 2

---
+++ day 5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var lines = File.ReadAllLines("data.txt").Select( x=> Convert.ToInt16(x)).ToArray();
+            var i =0;
+            var count = 0;
+            while(i < lines.Length)
+            {
+                var oldI = i;
+                i+=lines[i];
+
+                if(lines[oldI] <3) //comment for part 1
+                lines[oldI] ++;
+                else // comment for part 1
+                lines[oldI] --; // comment for part 1
+
+                count ++;
+            }
+
+        }
+    }
+}
