24ce7274647222fe9d995c7f6427cee000e70926
Sławomir Przybycień <SPrzybycien@Metegrity.com>
2017-12-08 11:48:07 +0100

    day 6 part 2

--- day 6/Program.cs
+++ day 6/Program.cs
@@ -8,11 +8,11 @@ namespace day_6
         static void Main(string[] args)
         {
             var banks = new int[]{4,10,4,1,8,4,9,14,5,1,14,15,0,15,3,5};
-            var existedConfigs = new HashSet<String>();
+            var existedConfigs = new Dictionary<int,String>();
             var cycleNo = 0;
-            while(!existedConfigs.Contains(String.Join(";",banks)))
+            while(!existedConfigs.ContainsValue(String.Join(";",banks)))
             {
-                existedConfigs.Add(String.Join(";",banks));
+                existedConfigs.Add(cycleNo,String.Join(";",banks));
                 var index =  Array.IndexOf(banks,banks.Max());
 
                 var memories = banks[index];
@@ -24,7 +24,9 @@ namespace day_6
                 }
                 cycleNo++;
             }
+            var firstOccur = existedConfigs.First(x=> x.Value == String.Join(";",banks));
 
+            var diff = cycleNo - firstOccur.Key;
         }
         
     }
