ac217eb4e7df47669aa28e3372d1f9988ca12782
sprz <sprzybycien@gmail.com>
2017-12-09 09:07:11 +0100

    day 9

---
+++ day 9/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+namespace day_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var chars = File.ReadAllText("data.txt");
+
+           var actualGroupOpened = 0;
+           var isGarbageOpened = false;
+           var score = 0;
+           var IsCancelled = false;
+           var notCancelled = 0;
+           foreach(var character in chars)
+           {
+                if(IsCancelled)
+                {
+                    IsCancelled = false;
+                     continue;
+                }
+
+                if(character == '!')
+                {
+                    IsCancelled = true;
+                    continue;
+                }
+
+                
+
+                if(isGarbageOpened)
+                    if(character == '>')
+                       {
+                            isGarbageOpened = false;
+                            continue;
+                       }
+                    else
+                    {
+                        notCancelled ++;
+                        continue;
+                    }
+
+                if(character == '<')
+                    isGarbageOpened = true;
+
+                if(character == '{')
+                    actualGroupOpened++;
+
+                if(character == '}')
+                    {
+                        score +=actualGroupOpened;
+                        actualGroupOpened --;
+                    }
+                
+           }
+           Console.WriteLine("SCORE: " + score);
+           Console.WriteLine("Not Cancelled: " + notCancelled);
+        }
+    }
+}
