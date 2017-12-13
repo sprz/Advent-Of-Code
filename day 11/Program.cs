f7e75b5bf5003d6fde4dde349970dea567391380
Sławomir Przybycień <SPrzybycien@Metegrity.com>
2017-12-13 15:22:37 +0100

    day 11 part 1

---
+++ day 11/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+namespace day_11
+{
+    class Program
+    {
+        private static Dictionary<string,Func<Point,Point>> dict = new Dictionary<string, Func<Point,Point>>(){
+            {"n",(point) => {return new Point(point.X, point.Y+ 2);} },
+            {"nw",(point) => {return new Point(point.X - 1,point.Y + 1);} },
+            {"ne",(point) => {return new Point(point.X + 1,point.Y + 1);} },
+            {"s",(point) => {return new Point(point.X, point.Y - 2);} },
+            {"sw",(point) => {return new Point(point.X - 1,point.Y - 1);} },
+            {"se",(point) => {return new Point(point.X + 1,point.Y - 1);} },
+        };
+        static void Main(string[] args)
+        {
+            var directions = File.ReadAllText("data.txt").Split(",");
+
+            Point currentPos = new Point();
+            
+            foreach(var direction in directions)
+            {
+                currentPos = dict[direction](currentPos);
+            }
+
+            currentPos = new Point(currentPos.Y,currentPos.X);
+            Console.WriteLine("Dist = " + DistToStart(currentPos));
+
+        }
+
+
+        public static int DistToStart(Point point)
+        {
+                if(Math.Abs(point.X) > Math.Abs(point.Y))
+                {
+                    return Math.Abs(point.X);
+                }
+                else
+                {
+                    return Math.Abs(point.X+point.Y)/ 2;
+                }
+        }
+    }
+}
