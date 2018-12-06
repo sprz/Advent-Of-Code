0d5e916157b98159177ae70ce3dfb9c4a75d3e46
sprz <sprzybycien@gmail.com>
2018-12-06 22:11:51 +0100

    day5 part 2

--- 2018/day05/Program.cs
+++ 2018/day05/Program.cs
@@ -20,6 +20,7 @@ namespace day_5
         {
             for(int i =path.Length-1; i > 0; i--)
             {
+                if(i >=path.Length) i--;
                 if(AreReacting(path[i-1],path[i]))
                 {
                     path  = path.Remove(i-1,2);
@@ -30,18 +31,26 @@ namespace day_5
 
              return path.Length;
         }
+        public static int Part2(string path)
+        {
+            int best = path.Length +1;
+            
+            char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            for(int i =0 ; i < alpha.Length;i++)
+            {
+                var charAsString = alpha[i].ToString();
+                var copiedPath = (string)path.Clone();
+                var pathWithRemoved = copiedPath.Replace(charAsString,"").Replace(charAsString.ToString().ToLower(),"");
+                var result = Part1(pathWithRemoved);
+                if(best > result) best = result;
+            }
+             return best;
+        }
+
         private static bool AreReacting(char a, char b)
         {
             return (a.ToString()).ToUpper() == (b.ToString()).ToUpper() && a != b;
             //return ((int)a - (int)b) % 32 == 0;
-        }
-        public static int Part2(string path)
-        {
-            for(int i =0 ; i < path.Length;i++)
-            {
-
-            }
-             return path.Length;
         }
     }
 }
