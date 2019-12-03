61691aa593aee52972bb33fce717a4706c09ede8
sprz <sprzybycien@gmail.com>
2019-12-03 08:09:40 +0100

    2019/03/2

--- 2019/day03/Program.cs
+++ 2019/day03/Program.cs
@@ -12,6 +12,7 @@ namespace day03
         {
             var text = System.IO.File.ReadAllLines("data.txt");
             Console.WriteLine("part1: " + part1(text));
+            Console.WriteLine("part2: " + part2(text));
         }
          
         static int part1(string[] lines)
@@ -52,6 +53,58 @@ namespace day03
                                 var manhDist = Math.Abs(centralPoint.X) + Math.Abs(centralPoint.Y);
                                 if(lowestManhattanDist > manhDist || lowestManhattanDist == -1)
                                     lowestManhattanDist = manhDist;
+                            }
+                        }
+                            
+                    }
+                }
+
+                isSecondWire = true;
+            }
+
+            
+            return lowestManhattanDist;
+        }
+
+         
+        static int part2(string[] lines)
+        {
+            var grid = new Dictionary<string,Tuple<bool,int>>();
+
+            int lowestManhattanDist = -1;
+            bool isSecondWire = false;
+            foreach(var wire in lines)
+            {
+                var centralPoint = new Point();
+                    var steps = 0;
+                foreach(var path in wire.Split(","))
+                {
+                    var direction = path.First();
+                    var length = Convert.ToInt16(path.Substring(1));
+              
+                    for(int i = 0; i < length; i++)
+                    {
+                        if(direction == 'R')
+                            centralPoint.X +=1;
+                        else if(direction == 'L')
+                            centralPoint.X -=1;
+                        else if(direction == 'U')
+                            centralPoint.Y +=1;
+                        else if(direction == 'D')
+                            centralPoint.Y -=1;
+                        steps++;
+                        var key = GetKey(centralPoint);
+
+                        if(grid.ContainsKey(key) == false)
+                        {
+                            grid.Add(key,new Tuple<bool,int>(isSecondWire,steps));
+                        }
+                        else
+                        {
+                            if(grid[key].Item1 != isSecondWire)
+                            {
+                                if(lowestManhattanDist > (steps + grid[key].Item2) || lowestManhattanDist == -1)
+                                    lowestManhattanDist = (steps + grid[key].Item2);
                             }
                         }
                             
