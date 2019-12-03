dc3c916bdb2e9c415e7d05af0999602d14a76e8e
sprz <sprzybycien@gmail.com>
2019-12-03 08:01:51 +0100

    2019/03/1

--- 2019/day03/Program.cs
+++ 2019/day03/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Numerics;
 
 namespace day03
 {
@@ -6,7 +10,66 @@ namespace day03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var text = System.IO.File.ReadAllLines("data.txt");
+            Console.WriteLine("part1: " + part1(text));
         }
+         
+        static int part1(string[] lines)
+        {
+            var grid = new Dictionary<string,bool>();
+
+            int lowestManhattanDist = -1;
+            bool isSecondWire = false;
+            foreach(var wire in lines)
+            {
+                var centralPoint = new Point();
+                foreach(var path in wire.Split(","))
+                {
+                    var direction = path.First();
+                    var length = Convert.ToInt16(path.Substring(1));
+              
+                    for(int i = 0; i < length; i++)
+                    {
+                        if(direction == 'R')
+                            centralPoint.X +=1;
+                        else if(direction == 'L')
+                            centralPoint.X -=1;
+                        else if(direction == 'U')
+                            centralPoint.Y +=1;
+                        else if(direction == 'D')
+                            centralPoint.Y -=1;
+
+                        var key = GetKey(centralPoint);
+
+                        if(grid.ContainsKey(key) == false)
+                        {
+                            grid.Add(key,isSecondWire);
+                        }
+                        else if(isSecondWire)
+                        {
+                            if(grid[key] != isSecondWire)
+                            {
+                                var manhDist = Math.Abs(centralPoint.X) + Math.Abs(centralPoint.Y);
+                                if(lowestManhattanDist > manhDist || lowestManhattanDist == -1)
+                                    lowestManhattanDist = manhDist;
+                            }
+                        }
+                            
+                    }
+                }
+
+                isSecondWire = true;
+            }
+
+            
+            return lowestManhattanDist;
+        }
+
+        static string GetKey(Point point){
+            return point.X + "," + point.Y;
+        }
+    }
+    public class Grid {
+        
     }
 }
