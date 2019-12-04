9a2ac2f36fbfe0624e0cfa38d2555e822d27f65f
Slawomir Przybycien <slawomir.przybycien@prosoftlab.pl>
2019-12-04 08:51:31 +0100

    2019/day04/1

---
+++ 2019/day04/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace day04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = 372037;
+            var end = 905157;
+            Console.WriteLine("part1: " + part1(start,end));
+        }
+        static int part1(int start, int end)
+        {
+            var counter = 0;
+
+            for(int i =start;i < end;i++)
+            {
+                var twoAdjacentdigits = false;
+                var isDecreasing = true;
+                var literal = i.ToString();
+                char previousChar = literal[0];
+                for(int k = 1; k < literal.Count() && isDecreasing;k++)
+                {
+                    if(literal[k-1] <= literal[k]) 
+                        continue;
+                    isDecreasing = false;
+                }
+                if(isDecreasing == false) continue;
+
+                previousChar = literal[0];
+                for(int k = 1; k < literal.Count() && !twoAdjacentdigits;k++)
+                {
+                    if(previousChar== literal[k]) 
+                        twoAdjacentdigits = true;
+                    else 
+                        previousChar = literal[k];
+                }
+                
+                if(twoAdjacentdigits) counter++;
+
+            }
+
+            return counter;
+        }
+    }
+}
