87fec74b2f117d15db2583f42ed82f1ed11ebe97
sprz <sprzybycien@gmail.com>
2019-12-08 15:16:06 +0100

    2019/day07 initial

---
+++ 2019/day07/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace day07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = System.IO.File.ReadAllText("data.txt");
+            // Console.WriteLine(part1(text));
+            // Console.WriteLine(part2(text));
+        }
+
+    }
+}
