9d4ed61500d24ca07381368481802cac1a470d01
sprz <sprzybycien@gmail.com>
2019-12-08 15:14:08 +0100

    2019/day06/2

--- 2019/day06/Program.cs
+++ 2019/day06/Program.cs
@@ -9,11 +9,11 @@ namespace day06
         static void Main(string[] args)
         {
             var text = System.IO.File.ReadAllText("data.txt");
-            Console.WriteLine(part1(text, 1));
-            Console.WriteLine(part2(text,5));
+            Console.WriteLine(part1(text));
+            Console.WriteLine(part2(text));
         }
         
-        static int part1(string text, int id)
+        static int part1(string text)
         {
             //text = testinput;
             var orbitDict = new Dictionary<string, Orbit>();
@@ -43,10 +43,41 @@ namespace day06
         }
 
         
-        static int part2(string text, int id)
+        
+        static int part2(string text)
         {
-            return 0;
+            //text = testinput;
+            var orbitDict = new Dictionary<string, Orbit>();
+
+            var lines = text.Split(Environment.NewLine).Select(x=> x.Split(')')).ToList();
+            foreach(var line in lines)
+            {
+                Orbit parent;
+                Orbit child;
+                if(!orbitDict.TryGetValue(line[0],out parent))
+                {
+                    parent = new Orbit(){Name = line[0]};
+                    orbitDict.Add(line[0],parent);
+                }
+                if(!orbitDict.TryGetValue(line[1],out child))
+                {
+                    child =  new Orbit(){Name = line[1]};
+                    orbitDict.Add(line[1],child);
+                }
+                child.Parent = parent;
+
+            }
+            var sanOrbitCounts = orbitDict["SAN"].GetParentNames().Split(',').ToList();
+            var youOrbitCounts = orbitDict["YOU"].GetParentNames().Split(',').ToList();
+
+            while(sanOrbitCounts[0] == youOrbitCounts[0])
+            {
+                sanOrbitCounts.RemoveAt(0);
+                youOrbitCounts.RemoveAt(0);
+            }
+            return sanOrbitCounts.Count + youOrbitCounts.Count -2;
         }
+
          class Orbit
         {
             public string Name {get;set;}
@@ -57,6 +88,13 @@ namespace day06
                 if(Parent == null) 
                     return 0;
                 return Parent.CountOrbits() +1;
+            }
+
+            public string GetParentNames()
+            {
+                if(Parent ==null)
+                    return "";
+                return Parent.GetParentNames() + ","+this.Name;
             }
 
         }
@@ -72,6 +110,8 @@ G)H
 D)I
 E)J
 J)K
-K)L";
+K)L
+K)YOU
+I)SAN";
     }
 }
