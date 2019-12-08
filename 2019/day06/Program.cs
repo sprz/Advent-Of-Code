fb2d1e5ebc3594fa18f3bbeafe777a76a01b4ebb
sprz <sprzybycien@gmail.com>
2019-12-08 15:05:03 +0100

    2019/day06/1

--- 2019/day06/Program.cs
+++ 2019/day06/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace day06
 {
@@ -6,7 +8,70 @@ namespace day06
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var text = System.IO.File.ReadAllText("data.txt");
+            Console.WriteLine(part1(text, 1));
+            Console.WriteLine(part2(text,5));
         }
+        
+        static int part1(string text, int id)
+        {
+            //text = testinput;
+            var orbitDict = new Dictionary<string, Orbit>();
+
+            var lines = text.Split(Environment.NewLine).Select(x=> x.Split(')')).ToList();
+            foreach(var line in lines)
+            {
+                Orbit parent;
+                Orbit child;
+                if(!orbitDict.TryGetValue(line[0],out parent))
+                {
+                    parent = new Orbit(){Name = line[0]};
+                    orbitDict.Add(line[0],parent);
+                }
+                if(!orbitDict.TryGetValue(line[1],out child))
+                {
+                    child =  new Orbit(){Name = line[1]};
+                    orbitDict.Add(line[1],child);
+                }
+                child.Parent = parent;
+
+            }
+
+            var output = orbitDict.Sum(x=> x.Value.CountOrbits());
+
+            return 0;
+        }
+
+        
+        static int part2(string text, int id)
+        {
+            return 0;
+        }
+         class Orbit
+        {
+            public string Name {get;set;}
+            public Orbit Parent {get;set;}
+
+            public int CountOrbits()
+            {
+                if(Parent == null) 
+                    return 0;
+                return Parent.CountOrbits() +1;
+            }
+
+        }
+
+
+        static string testinput = @"COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L";
     }
 }
