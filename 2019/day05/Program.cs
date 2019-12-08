0ba4be21753b4ba1254810341041d9bbd55c18f8
sprz <sprzybycien@gmail.com>
2019-12-08 14:49:16 +0100

    2019/day05/2

--- 2019/day05/Program.cs
+++ 2019/day05/Program.cs
@@ -9,7 +9,7 @@ namespace day05
         {
             var text = System.IO.File.ReadAllText("data.txt");
             Console.WriteLine(part1(text, 1));
-            // Console.WriteLine(part2(text));
+            Console.WriteLine(part2(text,5));
         }
         
         static int part1(string text, int id)
@@ -65,6 +65,97 @@ namespace day05
 
             return paramoutput;
         }
+
+        
+        static int part2(string text, int id)
+        {
+           // text = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99";
+            
+            var paramoutput = 0;
+            var values = text.Split(',').Select(x=> Convert.ToInt32(x)).ToList();
+
+            for(int i =0; i<values.Count();)
+            {
+                if(values[i] == 99) 
+                    break;
+
+                var paramModes = GetParamModes(values[i]);
+                var opcode = paramModes[0];
+                int firstValue=0, secondValue=0,thirdValue=0;
+
+                firstValue = values[i+1];
+
+                if(opcode != 3 && opcode != 4)
+                {
+                    secondValue = values[i+2];
+                    thirdValue = values[i+3];
+                    if(paramModes[1] == 0)
+                    firstValue = values[firstValue];
+                    if(paramModes[2] == 0)
+                    secondValue = values[secondValue];  
+                }           
+
+                if(opcode == 1)
+                {
+                    values[thirdValue] = firstValue+secondValue;
+                    i+=4;
+                }
+                else if(opcode == 2)
+                {
+                    values[thirdValue] = firstValue*secondValue;
+                    i+=4;
+                }
+                else if(opcode == 3)
+                {
+                    values[firstValue] = id;
+                    i+=2;
+                }
+                else if(opcode == 4)
+                {
+                    if(values[values[i+1]] != 0)
+                    {
+                        paramoutput = values[values[i+1]];
+                        Console.WriteLine(i + ": " + paramoutput);
+                    }
+                    i+=2;
+                }
+                else if(opcode == 5)
+                {
+                    if(firstValue != 0)
+                        i=secondValue;
+                    else
+                        i+=3;
+
+                }
+                else if(opcode == 6)
+                {
+                    if(firstValue == 0)
+                        i=secondValue;
+                    else
+                        i+=3;
+                }
+                else if(opcode == 7)
+                {
+                    if(firstValue < secondValue)
+                        values[thirdValue] = 1;
+                    else
+                        values[thirdValue] = 0;
+                    i+=4;
+                }
+                else if(opcode == 8)
+                {
+                    if(firstValue == secondValue)
+                        values[thirdValue] = 1;
+                    else
+                        values[thirdValue] = 0;
+                    i+=4;
+                }
+                
+            }
+
+            return paramoutput;
+        }
+
 
         private static int[] GetParamModes(int paramValue)
         {
