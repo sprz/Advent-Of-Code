dfabb517c83f48ae5bb84c7f61b7ec56809dc6f6
sprz <sprzybycien@gmail.com>
2020-12-06 15:34:27 +0100

    day04 part1

--- 2020/day04/day04.go
+++ 2020/day04/day04.go
@@ -4,11 +4,60 @@ import (
 	"bufio"
 	"fmt"
 	"log"
-    "os"
+	"os"
+	"strings"
 )
 
 func part1()  {
-    fmt.Println("part1: ",1);
+	var valid = 0
+	var file = readFile()
+
+	var hasByr = false;
+	var hasIyr = false
+	var hasEyr = false
+	var hasHgt = false
+	var hasHcl = false
+	var hasEcl = false
+	var hasPid = false
+
+	var currentRow = 0
+
+	for {
+		s := strings.Fields(string(file[currentRow]))
+		
+		if len(s)>0	{
+			for _, part := range s {
+				hasByr = hasByr || strings.HasPrefix(part, "byr")
+				hasIyr = hasIyr || strings.HasPrefix(part, "iyr")
+				hasEyr = hasEyr || strings.HasPrefix(part, "eyr")
+				hasHgt = hasHgt || strings.HasPrefix(part, "hgt")
+				hasHcl = hasHcl || strings.HasPrefix(part, "hcl")
+				hasEcl = hasEcl || strings.HasPrefix(part, "ecl")
+				hasPid = hasPid || strings.HasPrefix(part, "pid")
+			}
+		} else {
+
+			if hasByr && hasIyr && hasEyr && hasHgt && hasHcl && hasEcl && hasPid {
+				valid +=1
+			}
+
+			hasByr = false;
+			hasIyr = false
+			hasEyr = false
+			hasHgt = false
+			hasHcl = false
+			hasEcl = false
+			hasPid = false
+		}
+
+		currentRow +=1
+		if currentRow >=len(file){
+			break
+		}
+	}
+	
+
+    fmt.Println("part1: ",valid);
 }
 
 func part2()  {
