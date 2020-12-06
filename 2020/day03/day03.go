631090a3d6465f1e4b8326cd85dd2a2fb34e7ee5
sprz <sprzybycien@gmail.com>
2020-12-06 14:45:37 +0100

    day03 init

---
+++ 2020/day03/day03.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func part1()  {
+    fmt.Println("part1: ",1);
+}
+
+func part2()  {
+    fmt.Println("part2: ",2);
+}
+func main() {
+    part1()
+    part2()
+}
