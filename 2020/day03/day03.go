67dc58bc9dc4211dc29a8b60a791e16181be7900
sprz <sprzybycien@gmail.com>
2020-12-06 15:09:17 +0100

    day03 part2

--- 2020/day03/day03.go
+++ 2020/day03/day03.go
@@ -8,7 +8,74 @@ import (
 )
 
 func part1()  {
-    var valid = 0
+    var counter = 0
+
+
+	var txtlines = readFile()
+	var x=0
+	var y=0
+	
+	for {
+
+		if string(txtlines[y][x]) == "#"{
+			counter +=1
+		}
+
+		y+=1
+
+		if y >= len(txtlines) {
+			break
+		}
+
+		x=(x+3)%len(txtlines[y])
+	}
+
+
+    fmt.Println("part1: ",counter);
+}
+
+func part2()  {
+
+	var file = readFile()
+
+	var oneOne = countTrees(1,1,file)
+	var threeOne = countTrees(3,1,file)
+	var fiveOne = countTrees(5,1,file)
+	var sevenOne = countTrees(7,1,file)
+	var oneTwo = countTrees(1,2,file)
+
+	fmt.Println(oneOne)
+	fmt.Println(threeOne)
+	fmt.Println(fiveOne)
+	fmt.Println(sevenOne)
+	fmt.Println(oneTwo)
+	
+
+    fmt.Println("part2: ",oneOne*threeOne*fiveOne*sevenOne*oneTwo);
+}
+func countTrees(xMove int ,yMove int, mapp []string) int {
+	var counter = 0
+	var x=0
+	var y=0
+	for {
+
+		if string(mapp[y][x]) == "#"{
+			counter +=1
+		}
+
+		y+=yMove
+
+		if y >= len(mapp) {
+			break
+		}
+
+		x=(x+xMove)%len(mapp[y])
+	}
+	return counter;
+}
+
+
+func readFile() []string{
 	file, err := os.Open("data.txt")
 	if err != nil {
 		log.Fatalf("failed opening file: %s", err)
@@ -21,32 +88,7 @@ func part1()  {
 		txtlines = append(txtlines, scanner.Text())
 	}
 	file.Close()
-
-	var x=0
-	var y=0
-	
-	for {
-
-		if string(txtlines[x][y]) == "#"{
-			valid +=1
-			fmt.Println("X:",x,", Y:",y)
-		}
-
-		x+=1
-
-		if x >= len(txtlines) {
-			break
-		}
-
-		y=(y+3)%len(txtlines[x])
-	}
-
-
-    fmt.Println("part1: ",valid);
-}
-
-func part2()  {
-    fmt.Println("part2: ",2);
+	return txtlines
 }
 func main() {
     part1()
