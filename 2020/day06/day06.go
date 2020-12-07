98f8347ee63b661464f6a1afae413796d169c443
sprz <sprzybycien@gmail.com>
2020-12-07 18:38:50 +0100

    day06 part2

--- 2020/day06/day06.go
+++ 2020/day06/day06.go
@@ -32,9 +32,38 @@ func part1()  {
     fmt.Println("part1: ",counter);
 }
 
-func part2() {
-    fmt.Println("part2: ?");
+func part2() {	
+	var file = readFile()
+
+	var counter = 0
+	 var letters = [] rune{}
+	 var filteredLetters = [] rune{}
+	var isFirst = true
+
+	for i:=0;i<len(file);i++ {
+		
+		if len(file[i])>0	{
+			for _, letter := range file[i] {
+				if isFirst {
+					filteredLetters = append(filteredLetters,letter)
+				} else if contains(letters,letter){
+					filteredLetters = append(filteredLetters,letter)
+				}
+			}
+			letters = filteredLetters
+			filteredLetters = [] rune{}
+			isFirst = false
+		} else {
+			counter +=len(letters)
+			letters = [] rune{}
+			isFirst = true;
+		}
+	}
+	counter +=len(letters)
+
+    fmt.Println("part2: ",counter);
 }
+
 func  contains(s []rune,e rune) bool {
     for _, a := range s {
         if a == e {
