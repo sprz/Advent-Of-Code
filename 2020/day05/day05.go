14dbe5eff53ee3c6a29c86baef83c7cea5b5483a
sprz <sprzybycien@gmail.com>
2020-12-07 18:24:03 +0100

    day05 clean

--- 2020/day05/day05.go
+++ 2020/day05/day05.go
@@ -21,15 +21,14 @@ func part1()  {
 		}
 
 		for i:=0;i<3;i++{
-			if string(line[i+7]) == "B"{
-				col += int(math.Pow(2,float64(7-i-1)))
+			if string(line[i+7]) == "R"{
+				col += int(math.Pow(2,float64(3-i-1)))
 			}
 		}
 		var seatId = row * 8 + col;
 		
 		if maxSeatId < seatId{
 			maxSeatId = seatId;
-			println("row:", row, ", col:", col)
 		}
 		row = 0;
 		col = 0;
@@ -38,9 +37,54 @@ func part1()  {
 }
 
 func part2()  {
-    fmt.Println("part2: ",2);
+	var file = readFile();
+	var Ids = []int{}
+	var maxSeatId = 0;
+	for _, line := range file {
+		var row = 0;
+		var col = 0;
+		for i:=0;i<7;i++{
+			if string(line[i]) == "B"{
+				row += int(math.Pow(2,float64(7-i-1)))
+			}
+		}
+
+		for i:=0;i<3;i++{
+			if string(line[i+7]) == "R"{
+				col += int(math.Pow(2,float64(3-i-1)))
+			}
+		}
+		var seatId = row * 8 + col;
+		
+		Ids = append(Ids,seatId)
+		row = 0;
+		col = 0;
+		
+		if maxSeatId < seatId{
+			maxSeatId = seatId;
+		}
+	}
+
+	for i:=0; i<maxSeatId;i++{
+		var Id = i+1
+		if contains(Ids, Id) == false && contains(Ids, Id+1) && contains(Ids, Id-1) {
+			fmt.Println("part2: ",Id)
+			return
+		}
+	}
+	fmt.Println("part2: not found")
+
 }
 
+func  contains(s []int,e int) bool {
+    for _, a := range s {
+        if a == e {
+			// fmt.Println(a)
+            return true
+        }
+    }
+    return false
+}
 
 func readFile() []string{
 	file, err := os.Open("data.txt")
