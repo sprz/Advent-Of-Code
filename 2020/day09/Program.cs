2ecaf32e14a3d583c59f7353f4819fdeee3e5af7
sprz <sprzybycien@gmail.com>
2020-12-19 14:54:47 +0100

    day09 part1

---
+++ 2020/day09/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+namespace day09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("data.txt").Select( x=> long.Parse(x)).ToList();
+            part1(lines);
+        }
+        static void part1(List<long> numbers)
+        {
+            var output = 0;
+            for(int i=25;i<numbers.Count;i++)
+            {
+                var preambles = numbers.GetRange(i-25,25).OrderBy( x=> x);
+                var number = numbers[i];
+                
+                if(preambles.Any( x=> preambles.Any( y=> y + x == number))) continue;
+
+                Console.WriteLine("part1: " + numbers[i]);
+                return;
+            }
+        }
+    }
+}
