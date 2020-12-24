644fbf69b22460811b98511f7abb6a7109152ec0
sprz <sprzybycien@gmail.com>
2020-12-25 00:28:30 +0100

    day11 part2

--- 2020/day11/Program.cs
+++ 2020/day11/Program.cs
@@ -10,11 +10,28 @@ namespace day11
             var lines = File.ReadAllLines("data.txt");
             SeatMap map = SeatMap.Parse(lines);
             part1(map);
-            //part2(lines);
+            map = SeatMap.Parse(lines);
+            part2(map);
+            Console.ReadKey();
         }
 
         private static void part1(SeatMap map)
         {
+            map.IsPartOne = true;
+            do
+            {
+                map.PrepareNextStates();
+                map.DOIT();
+            }
+            while (map.SeatsOccupationChangedCount != 0);
+
+
+            Console.WriteLine("part1:" + map.OccupiedSeats);
+        }
+
+        private static void part2(SeatMap map)
+        {
+            map.IsPartOne = false;
             do
             {
                 map.PrepareNextStates();
--- 2020/day11/SeatMap.cs
+++ 2020/day11/SeatMap.cs
@@ -7,8 +7,13 @@ namespace day11
     internal class SeatMap
     {
         public Seat[,] Seats { get; set; }
+        private int xLen => Seats.GetLength(0);
+        private int yLen => Seats.GetLength(1);
         public int SeatsOccupationChangedCount => ListOfSeats.Count(x => x.StatusChanged);
         public int OccupiedSeats => ListOfSeats.Count(x => x.IsOccupied);
+
+        public bool IsPartOne { get; internal set; }
+
         public List<Seat> ListOfSeats = new List<Seat>();
 
         internal static SeatMap Parse(string[] lines)
@@ -27,44 +32,74 @@ namespace day11
         }
         internal void PrepareNextStates()
         {
-            var xLen = Seats.GetLength(0);
-            var yLen = Seats.GetLength(1);
-
             for(int i=0;i<xLen;i++)
                 for(int k=0;k<yLen;k++)
                 {
                     var seat = Seats[i, k];
                     if (seat.Occupied == null) continue;
 
-                    var xDown = i > 0;
-                    var xUp = i < xLen - 1;
-
-                    var yDown= k > 0;
-                    var yUp = k < yLen - 1;
-
                     var OccupiedCount = 0;
 
-                    if (xDown) OccupiedCount += Seats[i-1, k].IsOccupied ? 1 : 0;
-                    if (xUp) OccupiedCount += Seats[i+1, k].IsOccupied ? 1 : 0;
+                    if (IsPartOne)
+                    {
+                        var xDown = i > 0;
+                        var xUp = i < xLen - 1;
 
-                    if (yDown) OccupiedCount += Seats[i, k-1].IsOccupied ? 1 : 0;
-                    if (yUp) OccupiedCount += Seats[i, k+1].IsOccupied ? 1 : 0;
+                        var yDown = k > 0;
+                        var yUp = k < yLen - 1;
 
-                    if (xDown && yDown) OccupiedCount += Seats[i - 1, k-1].IsOccupied ? 1 : 0;
-                    if (xDown && yUp) OccupiedCount += Seats[i - 1, k+1].IsOccupied ? 1 : 0;
 
-                    if (xUp && yDown) OccupiedCount += Seats[i + 1, k-1].IsOccupied ? 1 : 0;
-                    if (xUp && yUp) OccupiedCount += Seats[i + 1, k+1 ].IsOccupied ? 1 : 0;
+                        if (xDown) OccupiedCount += Seats[i - 1, k].IsOccupied ? 1 : 0;
+                        if (xUp) OccupiedCount += Seats[i + 1, k].IsOccupied ? 1 : 0;
 
-                    if (seat.IsOccupied && OccupiedCount >= 4) seat.NextOccupiedStatus = false;
-                    if (seat.IsOccupied == false && OccupiedCount == 0) seat.NextOccupiedStatus = true;
+                        if (yDown) OccupiedCount += Seats[i, k - 1].IsOccupied ? 1 : 0;
+                        if (yUp) OccupiedCount += Seats[i, k + 1].IsOccupied ? 1 : 0;
 
+                        if (xDown && yDown) OccupiedCount += Seats[i - 1, k - 1].IsOccupied ? 1 : 0;
+                        if (xDown && yUp) OccupiedCount += Seats[i - 1, k + 1].IsOccupied ? 1 : 0;
+
+                        if (xUp && yDown) OccupiedCount += Seats[i + 1, k - 1].IsOccupied ? 1 : 0;
+                        if (xUp && yUp) OccupiedCount += Seats[i + 1, k + 1].IsOccupied ? 1 : 0;
+
+                        if (seat.IsOccupied && OccupiedCount >= 4) seat.NextOccupiedStatus = false;
+                        if (seat.IsOccupied == false && OccupiedCount == 0) seat.NextOccupiedStatus = true;
+                    }
+                    else
+                    {
+                        OccupiedCount += CheckOccupationInSpecificDirection(i, k, -1, 0) ? 1 : 0;
+                        OccupiedCount += CheckOccupationInSpecificDirection(i, k, 1, 0) ? 1 : 0;
+
+                        OccupiedCount += CheckOccupationInSpecificDirection(i, k, 0, -1) ? 1 : 0;
+                        OccupiedCount += CheckOccupationInSpecificDirection(i, k, 0, 1) ? 1 : 0;
+
+                        OccupiedCount += CheckOccupationInSpecificDirection(i, k, -1, -1) ? 1 : 0;
+                        OccupiedCount += CheckOccupationInSpecificDirection(i, k, -1, 1) ? 1 : 0;
+
+                        OccupiedCount += CheckOccupationInSpecificDirection(i, k, 1, -1) ? 1 : 0;
+                        OccupiedCount += CheckOccupationInSpecificDirection(i, k, 1, 1) ? 1 : 0;
+
+                        if (seat.IsOccupied && OccupiedCount >=5) seat.NextOccupiedStatus = false;
+                        if (seat.IsOccupied == false && OccupiedCount == 0) seat.NextOccupiedStatus = true;
+                    }
                 }
         }
 
         internal void DOIT()
         {
             ListOfSeats.Where( x=> x.Occupied != null).ToList().ForEach(x => x.UpdateOccupiedStatus());
+        }
+
+        private bool CheckOccupationInSpecificDirection(int x, int y, int xDirection, int yDirection)
+        {
+            while ((x += xDirection) >= 0 && x < xLen && (y += yDirection) >= 0 && y < yLen)
+            {
+                var nextSeat = Seats[x, y];
+
+                if (nextSeat.Occupied != null) return nextSeat.IsOccupied;
+            }
+            
+
+            return false;
         }
 
 
